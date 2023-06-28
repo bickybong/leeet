ea20d6cea31d6f6600cd431fc2ae89518aeae309
ze ff <bickybong@gmail.com>
2023-06-28 09:41:04 +0800

    maximum tree depth

--- Trees/104. Maximum Depth of Binary Tree.py
+++ Trees/104. Maximum Depth of Binary Tree.py
@@ -10,15 +10,31 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        depth = 0
-        curr = root
-        def DFS(depth, curr):
-            # print(curr, depth)
-            depth += 1
-            if not curr:
-                return depth - 1
-            return max(DFS(depth, curr.left), DFS(depth, curr.right))
-        return DFS(depth, curr)
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         # Recursive DFS, O(n) time and O(n) space
             
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
         
+        stack = [[root, 1]]
+        ans = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                ans = max(ans, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        
+        return ans
+        # Iterative DFS, O(n) time and O(n) space
